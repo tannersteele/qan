c7de4eb7ddf09fd43032479552cfaa4efd99e9b0
Tanner Steele <tannerjsteele@gmail.com>
2020-07-26 01:55:01 -0500

    Succesfully running React Native locally
    TODO: remove unnecessary files from VCS

---
+++ qan-react/qan/windows/qan/App.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "App.xaml.g.h"
+
+namespace winrt::qan::implementation
+{
+    struct App : AppT<App>
+    {
+        App() noexcept;
+    };
+} // namespace winrt::qan::implementation
+
+
