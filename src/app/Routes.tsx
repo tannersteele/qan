89129c6a22fdfc919658d2b98f01358fa548012a
Julien Couture <julien_couture@hotmail.com>
2020-08-22 11:32:17 -0400

    Fix routes that were still referencing counter

--- src/app/Routes.tsx
+++ src/app/Routes.tsx
@@ -10,17 +10,10 @@ const LazyCounterPage = React.lazy(() =>
   import(/* webpackChunkName: "CounterPage" */ './containers/CounterPage')
 );
 
-const CounterPage = (props: Record<string, any>) => (
-  <React.Suspense fallback={<h1>Loading...</h1>}>
-    <LazyCounterPage {...props} />
-  </React.Suspense>
-);
-
 export default function Routes() {
   return (
     <App>
       <Switch>
-        <Route path={routes.COUNTER} component={CounterPage} />
         <Route path={routes.HOME} component={HomePage} />
       </Switch>
     </App>
